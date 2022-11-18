03c5fb2c07da1387e77e2d58449513b5db1c053d
Deadmanch <stepanova.nata.r@gmail.com>
2022-11-18 17:05:31 +0200

    Доработал задание 3

--- lesson3/app.js
+++ lesson3/app.js
@@ -1,14 +1,11 @@
 const res = prompt('Сколько будет 7 + или - 15?');
 
 switch (true) {
-	case res === '22' :
-	case res === '-8' :
-		console.log('Успех')
-		break;
+	case Number(res) === 22 :
+	case Number(res) === -8 :
 	case res === 'Я не робот' :
 		console.log('Успех')
 		break;
-		
 	default :
 		console.log('Вы робот')
 }
